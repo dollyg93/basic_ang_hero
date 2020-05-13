7fd2b938ad86ef0c8e1c162704f9aff92870f24c
dollyg93 <dollygarg137@gmail.com>
2020-05-13 18:45:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wvzqvk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wvzqvk)
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+@NgModule({
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    AppRoutingModule, 
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeroesComponent,
+    HeroDetailsComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroSearchComponent
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
